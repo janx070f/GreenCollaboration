f919b906fce3d0041b7b1ab7bf1224b1eaa23a08
Jan Andreassen <janx070f@elev.techcollege.dk>
2017-03-06 12:52:57 +0100

    First

--- GreenCollaboration/Migrations/Configuration.cs
+++ GreenCollaboration/Migrations/Configuration.cs
@@ -16,60 +16,60 @@ namespace GreenCollaboration.Migrations
 
         protected override void Seed(GreenCollaboration.Models.ApplicationDbContext context)
         {
-            //    var layout = new CmsTemplate
-            //    {
-            //        Name = "Layout",
-            //        Alias = "layout"
-            //    };
-            //    var index = new CmsTemplate
-            //    {
-            //        Name = "Index",
-            //        Alias = "index"
-            //    };
+            var layout = new CmsTemplate
+            {
+                Name = "Layout",
+                Alias = "layout"
+            };
+            var index = new CmsTemplate
+            {
+                Name = "Index",
+                Alias = "index"
+            };
 
-            //    context.CmsTemplates.Add(layout);
-            //    context.CmsTemplates.Add(index);
+            context.CmsTemplates.Add(layout);
+            context.CmsTemplates.Add(index);
 
-            //    var defaultroot = new CmsPage
-            //    {
-            //        Name = "Default",
-            //        Alias = "default",
-            //        IconName = "",
-            //        Level = 0,
-            //        Order = 0,
-            //        Url = string.Empty,
-            //        ShowInMenu = false,
-            //        Template = layout
-            //    };
+            var defaultroot = new CmsPage
+            {
+                Name = "Default",
+                Alias = "default",
+                IconName = "",
+                Level = 0,
+                Order = 0,
+                Url = string.Empty,
+                ShowInMenu = false,
+                Template = layout
+            };
 
-            //    var indexpage = new CmsPage
-            //    {
-            //        Name = "Default",
-            //        Alias = "default",
-            //        Level = 1,
-            //        Order = 0,
-            //        Url = "/",
-            //        ShowInMenu = true,
-            //        Template = index,
-            //        IconName = "fa-home",
-            //        Parent = defaultroot
-            //    };
+            var indexpage = new CmsPage
+            {
+                Name = "Default",
+                Alias = "default",
+                Level = 1,
+                Order = 0,
+                Url = "/",
+                ShowInMenu = true,
+                Template = index,
+                IconName = "fa-home",
+                Parent = defaultroot
+            };
 
-            //    var prop = new CmsProperty
-            //    {
-            //        Name = "Textbox1",
-            //        Alias = "textbox1",
-            //        Type = "text",
-            //        Value = string.Empty,
-            //        Page = indexpage
+            var prop = new CmsProperty
+            {
+                Name = "Textbox1",
+                Alias = "textbox1",
+                Type = "text",
+                Value = string.Empty,
+                Page = indexpage
 
-            //    };
+            };
 
-            //    context.CmsPages.Add(defaultroot);
-            //    context.CmsPages.Add(indexpage);
-            //    context.CmsProperties.Add(prop);
+            context.CmsPages.Add(defaultroot);
+            context.CmsPages.Add(indexpage);
+            context.CmsProperties.Add(prop);
 
-            //    context.SaveChanges();
+            context.SaveChanges();
 
         }
     }
